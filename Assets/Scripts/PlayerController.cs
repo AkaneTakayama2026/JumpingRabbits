b0a71a38afce3c462ad23f4f01e18c7c02dfcc4a
AkaneTakayama2026 <uni202601_06@telework.to>
2026-06-09 10:58:54 +0900

    地面配置(仮)
    Tag(Platform)を各Platformに付けた

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //プレイヤーの移動速度
+    public float moveSpeed = 5f;
+    //ジャンプ力
+    public float jumpPower = 10f;
+
+    private Rigidbody2D rb;
+    //地面に接しているかどうか
+    private bool isGrounded = false;
+
+
+    void Start()
+    {
+        //rigidbody2Dを取得
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        //左右移動入力
+        float moveInput = 0f;
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            moveInput = -1f;
+        }
+
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            moveInput = 1f;
+        }
+        //左右移動を適用
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        //地面にいて、スペースキーが押されたらジャンプ
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            //空中状態に変更
+            isGrounded = false;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Platformに接触したら着地状態にする
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+        }
+    }
+}
