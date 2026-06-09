3677194f9195adda4cd877ea375fcdc82d7d339c
AkaneTakayama2026 <uni202601_06@telework.to>
2026-06-09 15:14:48 +0900

    足場が20→無限生成へ

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -1,60 +1,100 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
     public GameObject platformPrefab;
+    public Transform player;
 
-    public int platformCount = 20;
+    public int startPlatformCount = 20;
 
     public float startY = 2f;
 
-    public float minYSpacing = 1.5f;
-    public float maxYSpacing = 2f;
+    public float minYSpacing = 1.3f;
+    public float maxYSpacing = 1.8f;
 
-    public float minX = -4f;
-    public float maxX = 4f;
+    public float minX = -7f;
+    public float maxX = 7f;
 
-    public float minXDistance = 3f;
-    public float maxXDistance = 5f;
+    public float minXDistance = 4f;
+    public float maxXDistance = 6f;
 
-    private int nextDirection = 1;
+    public float generateAheadDistance = 20f;
+    public float deleteBelowDistance = 12f;
+
+    private List<GameObject> platforms = new List<GameObject>();
+
+    private Vector3 lastPosition;
+    private int nextDirection;
 
     void Start()
     {
-        Vector3 lastPosition = new Vector3(0f, startY, 0f);
-
-        Instantiate(platformPrefab, lastPosition, Quaternion.identity);
+        lastPosition = new Vector3(0f, startY, 0f);
+        CreatePlatform(lastPosition);
 
         nextDirection = Random.value < 0.5f ? -1 : 1;
 
-        for (int i = 1; i < platformCount; i++)
+        for (int i = 1; i < startPlatformCount; i++)
         {
-
-            if (lastPosition.x > maxX - minXDistance)
-            {
-                nextDirection = -1;
-            }
-            else if (lastPosition.x < minX + minXDistance)
-            {
-                nextDirection = 1;
-            }
-
-            float xDistance = Random.Range(minXDistance, maxXDistance) * nextDirection;
-
-            float x = lastPosition.x + xDistance;
-            x = Mathf.Clamp(x, minX, maxX);
-
-            float y = lastPosition.y + Random.Range(minYSpacing, maxYSpacing);
-
-            Vector3 newPosition = new Vector3(x, y, 0f);
-
-            Instantiate(platformPrefab, newPosition, Quaternion.identity);
-
-            lastPosition = newPosition;
-
-            nextDirection *= -1;
+            GenerateNextPlatform();
         }
-
     }
 
+    void Update()
+    {
+        if (player.position.y + generateAheadDistance > lastPosition.y)
+        {
+            GenerateNextPlatform();
+        }
+
+        DeleteOldPlatforms();
+    }
+
+    void GenerateNextPlatform()
+    {
+        if (lastPosition.x > maxX - minXDistance)
+        {
+            nextDirection = -1;
+        }
+        else if (lastPosition.x < minX + minXDistance)
+        {
+            nextDirection = 1;
+        }
+
+        float xDistance = Random.Range(minXDistance, maxXDistance) * nextDirection;
+
+        float x = lastPosition.x + xDistance;
+        x = Mathf.Clamp(x, minX, maxX);
+
+        float y = lastPosition.y + Random.Range(minYSpacing, maxYSpacing);
+
+        Vector3 newPosition = new Vector3(x, y, 0f);
+
+        CreatePlatform(newPosition);
+
+        lastPosition = newPosition;
+
+        nextDirection *= -1;
+    }
+
+    void CreatePlatform(Vector3 position)
+    {
+        GameObject newPlatform = Instantiate(platformPrefab, position, Quaternion.identity);
+        platforms.Add(newPlatform);
+    }
+
+    void DeleteOldPlatforms()
+    {
+        for (int i = platforms.Count - 1; i >= 0; i--)
+        {
+            if (platforms[i] == null) continue;
+
+            if (platforms[i].transform.position.y < player.position.y - deleteBelowDistance)
+            {
+                Destroy(platforms[i]);
+                platforms.RemoveAt(i);
+            }
+        }
+    }
 }
