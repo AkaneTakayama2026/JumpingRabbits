79da9a151ec6df30895536515512969017eafa42
AkaneTakayama2026 <uni202601_06@telework.to>
2026-06-16 14:12:58 +0900

    シーン遷移
    タイトル→1stStage→2ndStage→gameover

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+
+        ScoreManager.score = 0;
+
+        SceneManager.LoadScene("1stStageScene");
+    }
+}
